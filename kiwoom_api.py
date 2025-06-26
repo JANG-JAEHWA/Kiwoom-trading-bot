4dc5839a681055deb1bd9fe76489819216744e54
JANGJAEHWA <jaehwajang@yu.ac.kr>
2025-06-26 15:36:44 +0900

    feat(api): 일봉 데이터 조회(TR) 기능 추가

--- kiwoom_api.py
+++ kiwoom_api.py
@@ -8,11 +8,14 @@ class KiwoomAPI:
         self.api = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
         self._set_event_handlers()
         self.login_event_loop = QEventLoop()
+        self.tr_event_loop = QEventLoop()
+        self.tr_data = None
         self.account_number = None
 
     def _set_event_handlers(self):
         """이벤트와 이벤트 핸들러를 연결합니다."""
         self.api.OnEventConnect.connect(self._event_connect)
+        self.api.OnReceiveTrData.connect(self._recieve_tr_data)
 
     def login(self):
         """
@@ -36,3 +39,42 @@ class KiwoomAPI:
             print(f"로그인에 실패했습니다. 에러 코드: {err_code}")
 
         self.login_event_loop.exit()
+
+    def get_daily_data(self, code, data_str="20250625"):
+        """
+        지정한 종목의 일봉 데이터를 요청합니다.
+        TR 코드: opt10081
+        """
+        print(f"[{code}] 일봉 데이터 요청 중...")
+        
+        self.api.SetInputValue("종목코드", code)
+        self.api.SetInputValue("기준일자", data_str)
+        self.api.SetInputValue("수정주가구분", "1")#1: 수정주가, 0: 원주가
+
+        self.api.CommRqData("일봉데이터요청", "opt10081", 0, "0101")
+
+        self.tr_event_loop.exec_()
+
+        return self.tr_data
+
+    def _recieve_tr_data(self, screen_no, rqname, trcode, record_name, prev_next, *args):
+        """
+        TR 요청에 대한 응답 수신 이벤트 핸들러
+        """
+        if rqname == "일봉데이터요청":
+            print("일봉 데이터 수신 완료.")
+            count = self.api.GetRepeatCnt(trcode, rqname)
+            data_list = []
+            for i in range(count):
+                data = {
+                    'date': self.api.GetCommData(trcode, rqname, i, "일자").strip(),
+                    'open': int(self.api.GetCommData(trcode, rqname, i, "시가").strip()),
+                    'high': int(self.api.GetCommData(trcode, rqname, i, "고가").strip()),
+                    'low': int(self.api.GetCommData(trcode, rqname, i, "저가").strip()),
+                    'close': int(self.api.GetCommData(trcode, rqname, i, "현재가").strip()),
+                    'volume': int(self.api.GetCommData(trcode, rqname, i, "일자").strip())
+                }
+                data_list.append(data)
+            self.tr_data = data_list
+        self.tr_event_loop.exit()
+                    
